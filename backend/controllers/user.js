d1685573a13a1235b079d3be37fb164ee9a0d752
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-04 21:59:59 +0200

    correcció :

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -11,7 +11,7 @@ exports.signup = (req, res, next) => {
       password: hash
     });
     user.save()
-    .then(() => res.status(201).json({ message 'Utilisateur enregistré !' }))
+    .then(() => res.status(201).json({ message: 'Utilisateur enregistré !' }))
     .catch(error => res.status(400).json({ error }));
   })
   .catch(error => res.status(500).json({ error }));
