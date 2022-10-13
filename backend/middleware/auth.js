1b4e450f78dc0c7c6b204d1253ee5cdfcbf6fedd
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-13 15:50:51 +0200

    Connexió de MongoDB reparada

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, APP_SECRET);
+    const decodedToken = jwt.verify(token, process.env.CLAU_SECRETA);
     const userId = decodedToken.userId;
     req.auth = {
       userId: userId,
