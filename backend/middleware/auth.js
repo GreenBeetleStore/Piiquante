560d89c5d874b0f44bb496a78b365e4666386741
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-15 16:09:21 +0200

    Comentaris

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,10 +1,14 @@
 // in backend/middleware/auth.js
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 
 module.exports = (req, res, next) => {
   try {
+    // Captura del token.
     const token = req.headers.authorization.split(" ")[1];
+    // Verificar el token.
     const decodedToken = jwt.verify(token, process.env.PUBLIC_BAIT);
+    // Recuperar l'userID.
     const userId = decodedToken.userId;
     req.auth = {
       userId: userId,
